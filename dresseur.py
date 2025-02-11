c57820dd83fa88c2e90364e63dc610c4db514af6
yoel-settbon <yoel-settbon@laplateforme.io>
2025-02-11 12:09:52 +0100

    :construction: feat: addons class trainer

--- dresseur.py
+++ dresseur.py
@@ -1,3 +1,17 @@
+from pokemon import Pokemon
+
 class Trainer:
-    def __init__(self):
-        pass
+    def __init__(self, name, pokemons):
+        self.name = name
+        self.pokemons = [Pokemon(**p) for p in pokemons]
+    
+    def display_team(self):
+        return [pokemon.display_info() for pokemon in self.pokemons]
+    
+    def add_pokemons(self, pokemon):
+        self.pokemons.append(Pokemon(**pokemon))
+        return f"{self.name} obtained {pokemon['name']} !"
+    
+    def lose_pokemon(self, pokemon):
+        self.pokemons = [p for p in self.pokemons if p.name != pokemon.name]
+        return f"{self.name} lost {pokemon.name} !"
--- pokemon.py
+++ pokemon.py
@@ -10,9 +10,20 @@ class Pokemon:
         self.attacks = attacks
         self.evolution = evolution
 
-    def display_stats(self):
+    def display_info(self):
         info = f"{self.name} ({self.type}) - PV: {self.pv}/{self.pv_max}, ATK: {self.attack}, DEF: {self.defense}, VIT: {self.speed}\n"
         info += "Attacks :\n"
         for attack in self.attacks:
             info += f"  - {attack['name']} ({attack['type']}, Power: {attack['power']})\n"
         return info.strip()
+    
+    def attack(self, other_pokemon, attack_name):
+        attack = next((a for a in self.attacks if a["name"] == attack_name), None)
+        if attack:
+            damage = max(attack["power"] - other_pokemon.defense, 1)
+            other_pokemon.hp = max(other_pokemon.hp - damage, 0)
+            return f"{self.name} used {attack_name}! {other_pokemon.name} lost {damage} HP."
+        return f"{self.name} does not know this attack !"
+    
+    def is_ko(self):
+        return self.hp <= 0
