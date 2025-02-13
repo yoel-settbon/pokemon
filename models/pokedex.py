e050df989abc756ab168bf4065b26b398a5e8adf
yoel-settbon <yoel-settbon@laplateforme.io>
2025-02-13 11:00:14 +0100

    :construction: fix: pokedex.py fixed

--- models/pokedex.py
+++ models/pokedex.py
@@ -16,8 +16,5 @@ class Pokedex:
         return None
     
     def get_player_pokemon(self):
-<<<<<<< HEAD
         return [Pokemon(**p) for p in self.data["player_pokemon"]] 
-=======
         return [Pokemon(**p) for p in self.data["player_pokemon"]]
->>>>>>> origin/pre-game
