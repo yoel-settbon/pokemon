b1131916eac247e92d6406f26d091d071b69a331
yoel-settbon <yoel-settbon@laplateforme.io>
2025-02-13 11:07:20 +0100

    :construction: fix: pokemon.py fixed

--- models/pokemon.py
+++ models/pokemon.py
@@ -4,11 +4,8 @@ class Pokemon:
         self.type = type
         self.pv = pv
         self.pv_max = pv
-<<<<<<< HEAD
         self.attack = attack  # Garde l'attribut 'attack' pour la puissance de base
-=======
         self.attack = attack
->>>>>>> origin/pre-game
         self.defense = defense
         self.speed = speed
         self.attacks = attacks
@@ -21,7 +18,6 @@ class Pokemon:
             info += f"  - {attack['name']} ({attack['type']}, Power: {attack['power']})\n"
         return info.strip()
     
-<<<<<<< HEAD
     def perform_attack(self, other_pokemon, attack_name):  # Nouveau nom pour éviter le conflit
         attack = next((a for a in self.attacks if a["name"] == attack_name), None)
         if attack:
@@ -32,15 +28,3 @@ class Pokemon:
 
     def is_ko(self):
         return self.pv <= 0  # Correction de 'hp' en 'pv'
-=======
-    def attack(self, other_pokemon, attack_name):
-        attack = next((a for a in self.attacks if a["name"] == attack_name), None)
-        if attack:
-            damage = max(attack["power"] - other_pokemon.defense, 1)
-            other_pokemon.hp = max(other_pokemon.hp - damage, 0)
-            return f"{self.name} used {attack_name}! {other_pokemon.name} lost {damage} HP."
-        return f"{self.name} does not know this attack !"
-    
-    def is_ko(self):
-        return self.hp <= 0
->>>>>>> origin/pre-game
