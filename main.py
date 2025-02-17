e72f52f7b1631c74895a0fd3a2989c8885836e1f
jerome-cossu <jerome.cossu@laplateforme.io>
2025-02-17 15:23:41 +0100

    :sparkles: feat : addons misses chances

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ if __name__ == "__main__":
     menu_options = ['New Game', 'Load Game', 'Quit']
     menu = Menu(1100, 700, "assets/image/background.png", "assets/font/upheavtt.ttf", menu_options)
     menu.run()
+     
