18f7e95358f503751f3426bb291ecde4dbe53932
jerome-cossu <jerome.cossu@laplateforme.io>
2025-02-17 15:20:37 +0100

    :sparkles: feat : addons miss attack

--- models/game.py
+++ models/game.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from models.pokemon import Pokemon 
+from models.pokemon import Pokemon
 
 class Game:
     def __init__(self):
@@ -24,6 +24,11 @@ class Game:
         self.player_message = ""
         self.opponent_message = ""
 
+        self.last_attack_time = 0
+        self.attack_delay = 2000
+        self.show_player_message = True
+        self.message_display_time = 0
+
     def choose_pokemon(self):
         selected_index = 0
         self.display_pokemon_choice(selected_index)
@@ -42,6 +47,7 @@ class Game:
                     self.display_pokemon_choice(selected_index)
 
     def run(self):
+
         while True:
             running = True
             player_turn = True
@@ -52,6 +58,8 @@ class Game:
             self.opponent_message = ""
 
             while running:
+                current_time = pygame.time.get_ticks()
+
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         pygame.quit()
@@ -59,15 +67,30 @@ class Game:
                     elif event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_SPACE and player_turn:
                             damage = self.player_pokemon.attack_pokemon(self.opponent_pokemon)
-                            self.player_message = f"{self.player_pokemon.name} attack {self.opponent_pokemon.name} deals {damage} damages!"
+                            if damage == 0:
+                                self.player_message = f"{self.player_pokemon.name}'s attack missed!"
+                            else:
+                                self.player_message = f"{self.player_pokemon.name} attacks {self.opponent_pokemon.name} dealing {damage} damage!"
+                            self.last_attack_time = current_time
+                            self.show_player_message = True
+                            self.message_display_time = current_time + self.attack_delay
                             if self.opponent_pokemon.hp <= 0:
-                                self.player_message += f"{self.opponent_pokemon.name} is KO!"
+                                self.player_message += f" {self.opponent_pokemon.name} is KO!"
                                 running = False
                             player_turn = False
 
-                if not player_turn and self.opponent_pokemon.hp > 0:
+                if not player_turn and self.opponent_pokemon.hp > 0 and current_time >= self.message_display_time:
                     damage = self.opponent_pokemon.attack_pokemon(self.player_pokemon)
-                    self.opponent_message = f"{self.opponent_pokemon.name} attack {self.player_pokemon.name} deals {damage} damages!"
+
+                    if damage == 0:
+                        self.opponent_message = f"{self.opponent_pokemon.name}'s attack missed!"
+                    else:
+                        self.opponent_message = f"{self.opponent_pokemon.name} attacks {self.player_pokemon.name} dealing {damage} damage!"
+
+                    self.last_attack_time = current_time
+                    self.show_player_message = False
+                    self.message_display_time = current_time + self.attack_delay
+
                     if self.player_pokemon.hp <= 0:
                         self.opponent_message += f" {self.player_pokemon.name} is KO!"
                         running = False
@@ -79,20 +102,24 @@ class Game:
                 self.win.blit(text, (30, 500))
                 text = font.render(f"{self.opponent_pokemon.name} HP: {self.opponent_pokemon.hp}", True, self.WHITE)
                 self.win.blit(text, (860, 200))
-                player_message_text = font.render(self.player_message, True, self.WHITE)
-                self.win.blit(player_message_text, (30, 450))
-                opponent_message_text = font.render(self.opponent_message, True, self.WHITE)
-                self.win.blit(opponent_message_text, (50, 200))
-                pygame.display.flip()
 
-                pygame.time.delay(1000)
+                if self.show_player_message and current_time < self.message_display_time:
+                    player_message_text = font.render(self.player_message, True, self.WHITE)
+                    self.win.blit(player_message_text, (30, 450))
+
+                if not self.show_player_message and current_time < self.message_display_time:
+                    opponent_message_text = font.render(self.opponent_message, True, self.WHITE)
+                    self.win.blit(opponent_message_text, (30, 200))
+
+                pygame.display.flip()
 
             if self.player_pokemon.hp <= 0:
                 self.player_pokemon = self.choose_pokemon()
             else:
                 self.player_pokemon.reset_hp()
-                
+
     def display_pokemon_choice(self, selected_index):
+
         self.win.blit(self.background, (0, 0))
         font = pygame.font.Font(None, 36)
         text = font.render("Pick your Pokémon:", True, self.WHITE)
